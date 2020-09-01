40ec4066ec032469c8adacb3e67dcc534185bc50
shink32 <69580614+shink32@users.noreply.github.com>
2020-09-01 17:46:36 +0900

    プルリクエスト修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-         
+  
+  zenkaku = /\A[ぁ-んァ-ン一-龥]/
+  kana = /\A[ァ-ヶー－]+\z/
+  
   with_options presence: true do
   validates :nickname
-  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください。' }
-  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください。' }
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。' }
-  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。' }
+  validates :first_name, format: { with: zenkaku, message: 'は全角で入力してください。' }
+  validates :last_name, format: { with: zenkaku, message: 'は全角で入力してください。' }
+  validates :first_name_kana, format: { with: kana, message: 'は全角カタカナで入力して下さい。' }
+  validates :last_name_kana, format: { with: kana, message: 'は全角カタカナで入力して下さい。' }
   validates :birth_date
   end
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は英数字混同で入力してください' }
