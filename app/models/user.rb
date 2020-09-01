29ac08f5625340613501a9c9e62b538cc9a5ddf1
shink32 <69580614+shink32@users.noreply.github.com>
2020-09-01 12:02:02 +0900

    バリデーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+        #  with_options presence: true do
+          validates :nickname, presence: true
+          validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
+          validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
+          validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+          validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+          validates :birth_date, presence: true 
+
+          validates :email, presence: true
+          validates :password, presence: true 
+
 end
 
