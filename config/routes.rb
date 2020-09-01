7b9ac98b09454d31d6b3e535a370e8a9bad49a5c
shink32 <69580614+shink32@users.noreply.github.com>
2020-09-02 01:26:09 +0900

    【表示】views_items_new_form_withとscheduled_delivery

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "items#index"
   devise_for :users
   resources :users, only: [:edit, :update]
-  resources :items
+  resources :items, only: [:new, :create]
 
   
 end
