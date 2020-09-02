7b5f100aecbf7258445d74fa462f4f06f813f96d
shink32 <69580614+shink32@users.noreply.github.com>
2020-09-02 12:26:41 +0900

    ビュー記載(カテゴリ、商品状態)　collection_select

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,11 +19,11 @@ class Item < ApplicationRecord
   validates :scheduled_delivery
   end
 
-  validates :category,               numericality: { other_than: 1 } 
-  validates :sales_status,           numericality: { other_than: 1 } 
+  validates :category_id,            numericality: { other_than: 1 } 
+  validates :sales_status_id,        numericality: { other_than: 1 } 
   validates :shipping_fee_status_id, numericality: { other_than: 1 } 
   validates :prefecture_id,          numericality: { other_than: 1 } 
   validates :scheduled_delivery_id,  numericality: { other_than: 1 } 
 
-  
+
 end
