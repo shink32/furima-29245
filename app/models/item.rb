f287f42d90e48c0160e5ac304dfd765496ec0d06
shink32 <69580614+shink32@users.noreply.github.com>
2020-09-02 12:05:22 +0900

    カラム追加_prefecture

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,16 +2,22 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
-  belongs_to_active_hash [:scheduled_delivery, :shipping_fee_status]
+  belongs_to_active_hash [
+     :shipping_fee_status, 
+     :prefecture,
+     :scheduled_delivery
+    ]
 
 
   with_options presence: true do
-
-  validates :scheduled_delivery
   validates :shipping_fee_status
-
+  validates :prefecture
+  validates :scheduled_delivery
   end
 
-  validates :scheduled_delivery_id, numericality: { other_than: 1 } 
-  validates :scheduled_delivery_id, numericality: { other_than: 1 } 
+  validates :shipping_fee_status_id, numericality: { other_than: 1 } 
+  validates :prefecture_id,          numericality: { other_than: 1 } 
+  validates :scheduled_delivery_id,  numericality: { other_than: 1 } 
+
+
 end
