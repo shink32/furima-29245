4fbcc31a515120bd0f47ecbbe1fbf870d732ed47
shink32 <69580614+shink32@users.noreply.github.com>
2020-09-02 11:13:09 +0900

    カラム追加　shipping_fee_status

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,16 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
-  belongs_to_active_hash :scheduled_delivery
+  belongs_to_active_hash [:scheduled_delivery, :shipping_fee_status]
 
 
   with_options presence: true do
 
   validates :scheduled_delivery
+  validates :shipping_fee_status
 
   end
 
+  validates :scheduled_delivery_id, numericality: { other_than: 1 } 
   validates :scheduled_delivery_id, numericality: { other_than: 1 } 
 end
--- db/migrate/20200901101741_create_items.rb
+++ db/migrate/20200901101741_create_items.rb
@@ -1,12 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string     :name,                  null: false
-      t.integer    :price,                 null: false
-      t.text       :image,                 null: false
-      t.text       :comment,               null: false 
-      t.references :user,                  null: false, foreign_key: true
-      t.integer    :scheduled_delivery_id, null: false
+      t.string     :name,                   null: false
+      t.integer    :price,                  null: false
+      t.text       :image,                  null: false
+      t.text       :comment,                null: false 
+      t.references :user,                   null: false, foreign_key: true
+      t.integer    :scheduled_delivery_id,  null: false
+      t.integer    :shipping_fee_status_id, null: false
+      
       t.timestamps
     end
   end
