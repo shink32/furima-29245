da9064be5dce0e00bafcde3995fdfef4e53c40c0
shink32 <69580614+shink32@users.noreply.github.com>
2020-09-02 12:17:18 +0900

    アソシエーション,バリデーション設定(カテゴリ,商品状態)

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,8 @@ class Item < ApplicationRecord
 
   belongs_to :user
   belongs_to_active_hash [
+     :category,
+     :sales_status,
      :shipping_fee_status, 
      :prefecture,
      :scheduled_delivery
@@ -10,14 +12,18 @@ class Item < ApplicationRecord
 
 
   with_options presence: true do
+  validates :category
+  validates :sales_status
   validates :shipping_fee_status
   validates :prefecture
   validates :scheduled_delivery
   end
 
+  validates :category,               numericality: { other_than: 1 } 
+  validates :sales_status,           numericality: { other_than: 1 } 
   validates :shipping_fee_status_id, numericality: { other_than: 1 } 
   validates :prefecture_id,          numericality: { other_than: 1 } 
   validates :scheduled_delivery_id,  numericality: { other_than: 1 } 
 
-
+  
 end
