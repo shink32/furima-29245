903dff6612da78b12a84a4f7dc4eea81581495c5
shink32 <69580614+shink32@users.noreply.github.com>
2020-09-03 15:24:47 +0900

    数値のバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,12 +11,19 @@ class Item < ApplicationRecord
 
 
   with_options presence: true do
+  validates :image
+  validates :name
+  validates :comment
   validates :category
   validates :sales_status
   validates :shipping_fee_status
   validates :prefecture
   validates :scheduled_delivery
+  validates :price,             numericality: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください。'}
   end
+
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+
 
   validates :category_id,            numericality: { other_than: 1 } 
   validates :sales_status_id,        numericality: { other_than: 1 } 
