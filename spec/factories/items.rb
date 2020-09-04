8d5613fc5308cc1d92b335b41c1d0643b604d321
shink32 <69580614+shink32@users.noreply.github.com>
2020-09-04 12:31:29 +0900

    テストコード記載

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,15 +2,14 @@ FactoryBot.define do
   factory :item do
     name            {Faker::Name.initials(number: 2)}
     price           {'1000'}
-    comment         {'あああ'}
-
+    comment         {Faker::Lorem.sentence}
     sales_status_id   {'2'}
     category_id       {'2'}
     shipping_fee_status_id {'2'}
     prefecture_id          {'2'}
     scheduled_delivery_id  {'2'}
 
-    user_id  {'1'}
+    association :user
 
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,40 +10,68 @@ RSpec.describe Item, type: :model do
   it '画像がないと登録できない' do
     @item.image = nil
     @item.valid?
-    binding.pry
+    expect(@item.errors.full_messages).to include("Image can't be blank")
 
   end
 
   it '商品名が必須であること' do
+    @item.name = nil
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Name can't be blank")
   end
 
   it '商品の説明が必須であること' do
+    @item.comment = nil
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Comment can't be blank")
   end
 
   it 'カテゴリーの情報が必須であること' do
+    @item.category_id = nil
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
   end
 
   it '商品の状態についての情報が必須であること' do
+    @item.sales_status_id = nil
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Sales status can't be blank", "Sales status is not a number")
   end
 
   it '配送料の負担についての情報が必須であること' do
+    @item.shipping_fee_status_id = nil
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Shipping fee status can't be blank", "Shipping fee status is not a number")
   end
 
   it '発送元の地域についての情報が必須であること' do
+    @item.prefecture_id = nil
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Prefecture can't be blank", "Prefecture is not a number")
   end
 
   it '発送までの日数についての情報が必須であること' do
+    @item.scheduled_delivery_id = nil
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank", "Scheduled delivery is not a number")
   end
 
   it '価格についての情報が必須であること' do
+    @item.price = nil
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Price can't be blank", "Price は半角数字で入力してください。", "Price is out of setting range")
   end
 
   it '価格の範囲が、¥300~¥9,999,999の間であること' do
+    @item.price = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Price is out of setting range")
   end
 
   it '販売価格は半角数字のみ入力可能であること' do
+    @item.price = "１１１"
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Price は半角数字で入力してください。")
   end
-
-
 
 end
