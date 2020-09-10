0d4d36225b0560734035bc03bb1aa0fa3e437d23
shink32 <69580614+shink32@users.noreply.github.com>
2020-09-10 11:33:38 +0900

    URLを直接入力して購入済み商品の購入ページへ遷移しようとすると、トップページに遷移する
    @item.order.present?

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -43,6 +43,8 @@ end
 
   def move_to_top
     @item = Item.find(params[:item_id])
-    redirect_to items_path if current_user.id == @item.user_id
+    redirect_to items_path if (current_user.id == @item.user_id) || @item.order.present?
   end
+
+
 end
